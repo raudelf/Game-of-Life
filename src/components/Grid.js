04a31e11cab8736c8dcb0f7c46fe76c76fc34ff3
raudelf <rawdell@outlook.com>
2020-10-15 20:50:19 -0500

    :sparkles: Main.js & Grid.js
    Created the main page component which holds a simple title with the grid imported. The grid, for now, just has a div container with the text "Grid" inside to show where the grid will appear

---
+++ src/components/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grid = () => {
+    return(
+        <div className='gridContainer'>
+            <h3>Grid</h3>
+        </div>
+    )
+};
+
+export default Grid;
