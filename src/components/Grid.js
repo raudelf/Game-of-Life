818ea89b2100a4f0c0906342992029a883aea84a
raudelf <rawdell@outlook.com>
2020-10-19 23:00:35 -0500

    :sparkles: Grid Layout and Toggle
    I got the grid layed out and have functionality as far as clicking goes. Whenever you click on one of the boxes, you can toggle the color between blue and white.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import produce from 'immer';
+
+const numRows = 50;
+const numCols = 50;
 
 const Grid = () => {
+    const [grid, setGrid] = useState(() => {
+        const rows = [];
+
+        for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => 0))
+        }
+        
+        return rows
+    })
+
+    // console.log(grid)
     return(
         <div className='gridContainer'>
-            <h3>Grid</h3>
+            {grid.map((rows, i) => 
+                rows.map((col, j) => 
+                    <div
+                    className='gridBoxes'
+                    key={`${i}–${j}`}
+                    onClick={() => {
+                        const newGrid = produce(grid, gridCopy => {
+                            gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                        })
+                        setGrid(newGrid)
+                    }} 
+                    style={{
+                        backgroundColor: grid[i][j] ? 'blue' : undefined,
+                    }}/>
+                ))}
         </div>
     )
 };
