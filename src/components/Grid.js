f0f5e63ecabdb9e1699fb20f8b5f8e8e4aed79e4
Raudel Flores <rawdell@outlook.com>
2020-10-26 01:02:40 -0500

    :construction: START ERROR
    Start button is not reading the updated grid size when updated... still working to fix it to meet MVP

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,47 +1,130 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import produce from 'immer';
 
 // Buttons
 import StartBtn from './buttons/startBtn.js';
+import {operations} from './buttons/clearBtn.js';
 import RandomBtn from './buttons/randomBtn.js';
 import { generateEmptyGrid } from './buttons/clearBtn.js';
 
 
 const Grid = () => {
-    const [grid, setGrid] = useState(() => {        
-        return generateEmptyGrid()
+    
+    const [gridSize, setGridSize] = useState({
+        numRows: 50,
+        numCols: 50
     })
-
+    
+    const [grid, setGrid] = useState(() => {        
+        return generateEmptyGrid(gridSize.numRows, gridSize.numCols)
+    })
     const [generation, setGeneration] = useState(0)
+
+    // HERE
+    const [run, setRun] = useState(false)
+
+    const handleStart = () => {
+        setRun(!run);
+        if (!run) {
+            runRef.current = true;
+            handleRun()
+        }
+    }
+
+    const runRef = useRef(run);
+    runRef.current = run;
+
+    const handleRun = useCallback(() => {
+        if (!runRef.current) {
+            return;
+        }
+        console.log(gridSize.numRows, 'NUMROWS')
+        setGrid(grid => {
+            return produce(grid, gridCopy => {
+                for (let i = 0; i < gridSize.numRows; i++) {
+                    for (let j = 0; j < gridSize.numCols; j++) {
+                        let neighbors = 0;
+                        operations.forEach(([x, y]) => {
+                            const newI = i + x;
+                            const newJ = j + y;
+
+                            if (newI >= 0 && newI < gridSize.numRows && newJ >= 0 && newJ < gridSize.numCols) {
+                                neighbors += grid[newI][newJ]
+                            }
+                        })
+
+                        if (neighbors < 2 || neighbors > 3) {
+                            gridCopy[i][j] = 0;
+                        } else if (grid[i][j] === 0 && neighbors === 3) {
+                            gridCopy[i][j] = 1;
+                        }
+                    }
+                }
+            })
+        })
+
+        setGeneration(gen => {
+            return gen + 1
+        })
+        setTimeout(handleRun, 100);
+    }, []);
+    // END
 
     return(
         <>  
-            <StartBtn 
-            setGrid={setGrid}
-            setGeneration={setGeneration} />
+            <h3>Current Grid Dimensions: {gridSize.numRows}x{gridSize.numCols}</h3>
+        
+            <button onClick={handleStart}>{run ? 'Stop' : 'Start'}</button>
+
             <RandomBtn 
-            setGrid={setGrid} />
+            setGrid={setGrid}
+            gridSize={gridSize} />
+
             <button
             className='btn clearBtn'
             onClick={() => {
-                setGrid(generateEmptyGrid())
+                setGrid(generateEmptyGrid(gridSize.numRows, gridSize.numCols))
                 setGeneration(0)
             }}>Clear</button>
-            <div className='gridContainer'>
+
+            <p>Grid Size: </p>
+            <button onClick={() => {
+                setGridSize({...gridSize, numRows: 25, numCols: 25})
+                generateEmptyGrid(gridSize.numRows, gridSize.numCols)
+            }}>25x25</button>
+
+            <button onClick={() => {
+                setGridSize({...gridSize, numRows: 50, numCols: 50})
+                generateEmptyGrid(gridSize.numRows, gridSize.numCols)
+            }}>50x50</button>
+
+            <button onClick={() => {
+                setGridSize({...gridSize, numRows: 75, numCols: 75})
+                generateEmptyGrid(gridSize.numRows, gridSize.numCols)
+            }}>75x75</button>
+
+
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${gridSize.numCols}, 20px)`
+            }}>
                 {grid.map((rows, i) => 
                     rows.map((col, j) => 
                         <div
-                        className='gridBoxes'
                         key={`${i}–${j}`}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            backgroundColor: grid[i][j] ? 'blue' : undefined,
+                            border: 'solid 1px black'
+                        }}
                         onClick={() => {
                             const newGrid = produce(grid, gridCopy => {
                                 gridCopy[i][j] = grid[i][j] ? 0 : 1;
                             })
                             setGrid(newGrid)
                         }} 
-                        style={{
-                            backgroundColor: grid[i][j] ? 'blue' : undefined,
-                        }}/>
+                        />
                     ))}
             </div>
             <h3>Generation: {generation}</h3>
--- src/components/buttons/startBtn.js
+++ src/components/buttons/startBtn.js
@@ -1,12 +1,11 @@
-import React, {useState , useCallback, useRef} from 'react';
-import {operations, numRows, numCols} from './clearBtn.js';
+import React, {useState , useCallback, useRef, useEffect} from 'react';
+import {operations} from './clearBtn.js';
 import produce from 'immer';
 
 const StartBtn = (props) => {
     const [run, setRun] = useState(false)
 
-    const handleStart = e => {
-        e.preventDefault();
+    const handleStart = () => {
         setRun(!run);
         if (!run) {
             runRef.current = true;
@@ -21,17 +20,18 @@ const StartBtn = (props) => {
         if (!runRef.current) {
             return;
         }
-        
+        let gridDim = props.gridSize
+        console.log(gridDim.numRows, 'NUMROWS')
         props.setGrid(grid => {
             return produce(grid, gridCopy => {
-                for (let i = 0; i < numRows; i++) {
-                    for (let j = 0; j < numCols; j++) {
+                for (let i = 0; i < gridDim.numRows; i++) {
+                    for (let j = 0; j < gridDim.numCols; j++) {
                         let neighbors = 0;
                         operations.forEach(([x, y]) => {
                             const newI = i + x;
                             const newJ = j + y;
 
-                            if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                            if (newI >= 0 && newI < gridDim.numRows && newJ >= 0 && newJ < gridDim.numCols) {
                                 neighbors += grid[newI][newJ]
                             }
                         })
